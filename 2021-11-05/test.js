22862fed30b4a13280f9aaea1b4bddbf7bbc6b24
Hong-JunHyeok <edb1631@dgsw.hs.kr>
2021-11-05 22:07:33 +0900

    UPDATE: TIL

---
+++ 2021-11-05/test.js
@@ -0,0 +1,15 @@
+var http = require("http");
+
+var options = {
+  port: 8124,
+  path: "/?file=secondary",
+  method: "GET",
+};
+
+var processPublicTimeline = function (response) {
+  console.log("finished request");
+};
+
+for (var i = 0; i < 2000; i++) {
+  http.request(options, processPublicTimeline).end();
+}
